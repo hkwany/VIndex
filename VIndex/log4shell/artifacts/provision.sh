37757459ca6b1d659787a7def8a8bf68021ca08d
hkwany <hkwany@users.noreply.github.com>
2022-02-21 16:05:05 +0800

    Update provision.sh

--- VIndex/log4shell/artifacts/provision.sh
+++ VIndex/log4shell/artifacts/provision.sh
@@ -6,4 +6,6 @@ pip install --no-index --find-links $POC_PATH -r $POC_PATH/requirements.txt
 
 cd $POC_PATH && docker build -t log4j-shell-poc .
 
-cd $POC_PATH && zip -F jdk1.8.0_20.zip --out jdk1.8.0_20_full.zip && unzip jdk1.8.0_20_full.zip
+zip -F jdk1.8.0_20.zip --out jdk1.8.0_20_full.zip && unzip jdk1.8.0_20_full.zip
+
+chmod +x -R jdk1.8.0_20/
